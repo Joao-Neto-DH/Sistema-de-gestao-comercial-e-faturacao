21ad8920a3bdb8942907b3c9bf2c79878b398098
João Neto <joaolima882018@gmail.com>
2022-04-30 20:10:43 +0100

    Inserindo SideBar

--- lib/screens/main_screen_widget.dart
+++ lib/screens/main_screen_widget.dart
@@ -1,17 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:sistema_de_gestao_comercial/screens/side_bar_widget.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("OLA"),
-        // automaticallyImplyLeading: false,
-      ),
-      drawer: const SideBar(),
+    return Container(
+      child: const Text("welcome"),
     );
   }
 }
