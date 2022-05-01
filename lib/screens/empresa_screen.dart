ed7a56a39aec03be5df70151b4b5f724f9e4da79
João Neto <joaolima882018@gmail.com>
2022-05-01 14:14:49 +0100

    Adicionando EmpresaScreen

---
+++ lib/screens/empresa_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class EmpresaScreen extends StatelessWidget {
+  const EmpresaScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        child: Column(
+      children: [TextFormField()],
+    ));
+  }
+}
