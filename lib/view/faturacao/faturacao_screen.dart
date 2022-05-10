ff3dc1c6850b44dddd2fe97a3bce0bfe13b1ac81
Joao-Neto-DH <joaolima882018@gmail.com>
2022-05-10 20:05:56 +0100

    Tela de faturacao

---
+++ lib/view/faturacao/faturacao_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FaturacaoScreen extends StatelessWidget {
+  const FaturacaoScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
